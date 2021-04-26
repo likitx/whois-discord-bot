2f1ddb297076a96dce674814647a962f92b356a7
likitx <likitx@gmail.com>
2021-04-26 22:58:43 +0900

    202104262258

---
+++ Cogs/core.py
@@ -0,0 +1,110 @@
+# _*_ coding:utf-8 _*_
+import discord, asyncio, json, urllib.request, re
+from discord.ext import commands
+import os
+
+
+class UserInfo(commands.Cog):
+    def __init__(self, app):
+        self.app = app
+        
+        self.url_user_name = "https://gameinfo.albiononline.com/api/gameinfo/search?q="
+        self.url_user_id = "https://gameinfo.albiononline.com/api/gameinfo/players/"
+        
+    async def temp(self, ctx):
+        embed = discord.Embed(title="Char's info", description="", color=0xc16666)
+        embed.add_field(name="!who <char>", value="char's basic info", inline=True)
+        embed.add_field(name="!gi <char>", value="char's gathering info", inline=False)
+        embed.add_field(name="Important !", value="<char> is case sensitive.",inline=False)
+
+        #embed.set_footer(text="<char> is case sensitive.")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def who(self, ctx, arg = None):
+        if(arg == None):
+            await self.temp(ctx)
+            return None
+
+        if(arg != None):
+            arg = re.sub('[^A-Za-z0-9]','',arg)
+            
+            await ctx.send(f'{arg}? Hmm...')
+            
+            fullURL = self.url_user_name+arg
+            with urllib.request.urlopen(fullURL) as url:
+                data_load = json.loads(url.read().decode())
+
+            pName = data_load["players"][0]["Name"]
+            pGuild = data_load["players"][0]["GuildName"]
+            pAlliance = data_load["players"][0]["AllianceName"]
+            pTotal = data_load["players"][0]["KillFame"]
+            pKill = data_load["players"][0]["KillFame"]
+            pDeath = data_load["players"][0]["DeathFame"]
+            pRatio = data_load["players"][0]["FameRatio"]
+
+            if(pName == "" or pName != arg):
+                await ctx.send(f'{arg} is not found. <char> is case sensitive.')
+                return None
+                
+            if(pName == arg):
+                embed = discord.Embed(title=arg, description="", color=0xc16666)
+                embed.add_field(name="Guild", value=pGuild, inline=True)
+                embed.add_field(name="Alliance", value=pAlliance, inline=False)
+                embed.add_field(name="K/F:", value="{0:,}".format(pKill), inline=True)
+                embed.add_field(name="D/F:", value="{0:,}".format(pDeath), inline=True)
+                embed.add_field(name="Ratio:", value=pRatio, inline=True)
+
+                await ctx.send(embed=embed)
+                return None
+
+    @commands.command()
+    async def gi(self, ctx, arg = None):
+        if(arg == None):
+            await self.temp(ctx)
+            return None
+
+        if(arg != None):
+            arg = re.sub('[^A-Za-z0-9]','',arg)
+            
+            await ctx.send(f'{arg}? Hmm...')
+
+            fullURL = self.url_user_name+arg
+            with urllib.request.urlopen(fullURL) as url:
+                data_load = json.loads(url.read().decode())
+
+            pName = data_load["players"][0]["Name"]
+            pId = data_load["players"][0]["Id"]
+
+            if(pName == "" or pName != arg):
+                await ctx.send(f"\"{arg}\" is not found. <char> is case sensitive.")
+                
+            if(pName == arg):
+                embed = discord.Embed(title=arg, description="", color=0xc16666)
+
+                fullURL = self.url_user_id+pId
+                with urllib.request.urlopen(fullURL) as url:
+                    info_load = json.loads(url.read().decode())
+
+                pGTotal = info_load["LifetimeStatistics"]["Gathering"]["All"]["Total"]
+                pFiber = info_load["LifetimeStatistics"]["Gathering"]["Fiber"]["Total"]
+                pHide = info_load["LifetimeStatistics"]["Gathering"]["Hide"]["Total"]
+                pOre = info_load["LifetimeStatistics"]["Gathering"]["Ore"]["Total"]
+                pRock = info_load["LifetimeStatistics"]["Gathering"]["Rock"]["Total"]
+                pWood = info_load["LifetimeStatistics"]["Gathering"]["Wood"]["Total"]
+                pFishing = info_load["LifetimeStatistics"]["FishingFame"]
+                
+                gTotal = pGTotal + pFishing
+                embed.add_field(name="Total(resources+fishing) : ", value="{0:,}".format(gTotal), inline=False)
+                embed.add_field(name="Fiber:", value="{0:,}".format(pFiber), inline=True)
+                embed.add_field(name="Hide:", value="{0:,}".format(pHide), inline=True)
+                embed.add_field(name="Ore:", value="{0:,}".format(pOre), inline=True)
+                embed.add_field(name="Rock:", value="{0:,}".format(pRock), inline=True)
+                embed.add_field(name="Wood:", value="{0:,}".format(pWood), inline=True)
+                embed.add_field(name="Fishing:", value="{0:,}".format(pFishing), inline=False)
+                
+                await ctx.send(embed=embed)
+
+def setup(app):
+    app.add_cog(UserInfo(app))
+    
--- player_info.py
+++ player_info.py
@@ -1,112 +1,52 @@
 # _*_ coding:utf-8 _*_
-import discord, asyncio, json, requests, re
+import discord, asyncio, json, urllib.request, re
 from discord.ext import commands
 import os
 
-client = discord.Client()
+token = "ODM1ODIwNTY1MDkyNzYxNjYx.YIVAjw.OTbcvW0ogICrcgftF3ekQTzMNL0"
 
-bot = commands.Bot(command_prefix='!')
+app = commands.Bot(command_prefix="!")
 
-url_user_name = "https://gameinfo.albiononline.com/api/gameinfo/search?q="
-url_user_id = "https://gameinfo.albiononline.com/api/gameinfo/players/"
+# first loading cogs
+for filename in os.listdir("Cogs"):
+    if filename.endswith(".py"):
+        app.load_extension(f"Cogs.{filename[:-3]}")
 
-async def temp(ctx=None):
-    embed = discord.Embed(title="Char's info", description="", color=0xc16666)
-    embed.add_field(name="!who <char>", value="char's basic info", inline=True)
-    embed.add_field(name="!gi <char>", value="char's gathering info", inline=False)
-    embed.add_field(name="Important !", value="<char> is case sensitive.",inline=False)
 
-    #embed.set_footer(text="<char> is case sensitive.")
-    await ctx.send(embed=embed)
-
-@bot.event
+@app.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Hmm..."))
-    print("ready")
-       
-@bot.command()
-async def gi(ctx, arg = None):
-    if(arg == None):
-        #embed = discord.Embed(title="char's guild, alliance, etc.", description="usage : !gi [char name]\n[char name] is case sensitive.", color=0xc16666)
-        #await ctx.send(embed=embed)
-        await temp(ctx)
+    print(app.user.name)
+    print(app.user.id)
+    game = discord.Game("Hmm...");
+    await app.change_presence(status=discord.Status.online, activity=game)
 
-    if(arg != None):
-        arg = re.sub('[^A-Za-z0-9]','',arg)
-        
-        await ctx.send(f'{arg}? Hmm...')
+# load cogs
+@app.command()
+async def load_cogs(ctx, extension):
+    app.load_extension(f"Cogs.{extension}")
+    await ctx.send(f"{extension} is loaded")
 
-        r = requests.get(url_user_name+arg)
-        data = r.text
-        data_load = json.loads(data)
+# unload cogs
+@app.command()
+async def unload_cogs(ctx, extension):
+    app.unload_extension(f"Cogs.{extension}")
+    await ctx.send(f"{extension} is unloaded")
 
-        pName = data_load["players"][0]["Name"]
-        pId = data_load["players"][0]["Id"]
+# reload cogs
+@app.command()
+async def reload_cogs(ctx, extension=None):
+    if extension is None:
+        for filename in os.listdir("Cogs"):
+            if filename.endswith(".py"):
+                app.unload_extension(f"Cogs.{filename[:-3]}")
+                app.load_extension(f"Cogs.{filename[:-3]}")
+                await ctx.send("all extension is reloaded")
+    else:
+        app.unload_extension(f"Cogs.{extension}")
+        app.load_extension(f"Cogs.{extension}")
+        await ctx.send(f"{extension} is reloaded")
 
-        if(pName == "" or pName != arg):
-            await ctx.send(f"\"{arg}\" is not found. <char> is case sensitive.")
-            
-        if(pName == arg):
-            embed = discord.Embed(title=arg, description="", color=0xc16666)
-            s = requests.get(url_user_id+pId)
-            info = s.text
-            info_load = json.loads(info)
 
-            pGTotal = info_load["LifetimeStatistics"]["Gathering"]["All"]["Total"]
-            pFiber = info_load["LifetimeStatistics"]["Gathering"]["Fiber"]["Total"]
-            pHide = info_load["LifetimeStatistics"]["Gathering"]["Hide"]["Total"]
-            pOre = info_load["LifetimeStatistics"]["Gathering"]["Ore"]["Total"]
-            pRock = info_load["LifetimeStatistics"]["Gathering"]["Rock"]["Total"]
-            pWood = info_load["LifetimeStatistics"]["Gathering"]["Wood"]["Total"]
-            pFishing = info_load["LifetimeStatistics"]["FishingFame"]
-            
-            gTotal = pGTotal + pFishing
-            embed.add_field(name="Total(resources+fishing) : ", value="{0:,}".format(gTotal), inline=False)
-            embed.add_field(name="Fiber:", value="{0:,}".format(pFiber), inline=True)
-            embed.add_field(name="Hide:", value="{0:,}".format(pHide), inline=True)
-            embed.add_field(name="Ore:", value="{0:,}".format(pOre), inline=True)
-            embed.add_field(name="Rock:", value="{0:,}".format(pRock), inline=True)
-            embed.add_field(name="Wood:", value="{0:,}".format(pWood), inline=True)
-            embed.add_field(name="Fishing:", value="{0:,}".format(pFishing), inline=False)
-            
-            await ctx.send(embed=embed)
-@bot.command()
-async def who(ctx, arg = None):
-    if(arg == None):
-        await temp(ctx)
-        return None
-
-    if(arg != None):
-        arg = re.sub('[^A-Za-z0-9]','',arg)
-        
-        await ctx.send(f'{arg}? Hmm...')
-
-        r = requests.get(url_user_name+arg)
-        data = r.text
-        data_load = json.loads(data)
-
-        pName = data_load["players"][0]["Name"]
-        pGuild = data_load["players"][0]["GuildName"]
-        pAlliance = data_load["players"][0]["AllianceName"]
-        pTotal = data_load["players"][0]["KillFame"]
-        pKill = data_load["players"][0]["KillFame"]
-        pDeath = data_load["players"][0]["DeathFame"]
-        pRatio = data_load["players"][0]["FameRatio"]
-
-        if(pName == "" or pName != arg):
-            await ctx.send(f'{arg} is not found. <char> is case sensitive.')
-            return None
-            
-        if(pName == arg):
-            embed = discord.Embed(title=arg, description="", color=0xc16666)
-            embed.add_field(name="Guild", value=pGuild, inline=True)
-            embed.add_field(name="Alliance", value=pAlliance, inline=False)
-            embed.add_field(name="K/F:", value="{0:,}".format(pKill), inline=True)
-            embed.add_field(name="D/F:", value="{0:,}".format(pDeath), inline=True)
-            embed.add_field(name="Ratio:", value=pRatio, inline=True)
-
-            await ctx.send(embed=embed)
-            return None
-
-bot.remove_command("help")
-bot.run(str(os.environ.get('token')))
+#app.remove_command("help")
+app.run(token)
+#app.run(str(os.environ.get('token')))
